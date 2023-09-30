df296abb74680f84fb19a37159b772545d879f6a
dane <hello@itsdane.co>
2023-09-30 16:52:22 -0700

    adding starter React

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	<img src={logo} className="App-logo" alt="logo" />
+        <h1>Responsive Web App</h1>
+      </header>
+      <main>
+        <div className="content">
+          <h2>Welcome to our Web App</h2>
+          <p>This is a basic responsive React web application.</p>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Your Company</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
