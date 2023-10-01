a19aa7ba043afb1a19e9d095718eceabd4ebe7db
dane <hello@itsdane.co>
2023-09-30 17:01:04 -0700

    Updates

---
+++ precache-manifest.44f3d5b27010d6a82cdc6b78daeb4c63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd7f5309feeb8257141e",
+    "url": "/pizza-ui/static/css/main.67c02eba.chunk.css"
+  },
+  {
+    "revision": "fd7f5309feeb8257141e",
+    "url": "/pizza-ui/static/js/main.fd7f5309.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/pizza-ui/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/pizza-ui/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "ff9e640def9a7fccac78",
+    "url": "/pizza-ui/static/js/runtime~main.ff9e640d.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/pizza-ui/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "61fa956a73a0719415574fcc03cfd41d",
+    "url": "/pizza-ui/index.html"
+  }
+];
